1b78b448697aa4a2de68368c4b0dc583e8a99996
Mario Schmidt <mssoftware@outlook.de>
2020-12-05 06:22:45 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-docker build . --target=ubuntu-desktop --tag=mssoftware/ubuntu-desktop:latest
-docker push mssoftware/ubuntu-desktop
-
-docker build . --target=ubuntu-workspace --tag=mssoftware/ubuntu-workspace:latest
-docker push mssoftware/ubuntu-workspace
